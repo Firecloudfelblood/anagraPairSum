894c9d2586306f3e3a7f9358ac7741930eb2f76a
grimripper <>
2021-07-30 21:54:00 -0700

    romed a couple of warnings

--- PairSum/SumaPares.py
+++ PairSum/SumaPares.py
@@ -2,7 +2,7 @@ def suma_pares(arr, k):
     cardinalidad = 0
     container = []
     for num in arr:
-        for i in range(arr.index(num)+1, len(arr), 1):
+        for i in range(arr.index(num) + 1, len(arr), 1):
             if num + arr[i] == k and not [num, arr[i]] in container:
                 cardinalidad += 1
                 container.append([num, arr[i]])
--- anagram/anagramasgcm.py
+++ anagram/anagramasgcm.py
@@ -1,11 +1,11 @@
 def anagram(frase1, frase2):
-    '''
+    """
     toma 2 fraces las convierte a diccionario ignorando los espacios y guines
      y las compara para ver si son anagramas
     :param frase1:
     :param frase2:
     :return:
-    '''
+    """
     dic1 = create_dic(frase1)
     dic2 = create_dic(frase2)
 
@@ -15,7 +15,7 @@ def anagram(frase1, frase2):
     # print(dic1)
     # print(dic2)
 
-    #los prints dentro del if son para indicar que resultado de acuerdo alos valores y llaves
+    # los prints dentro del if son para indicar que resultado de acuerdo alos valores y llaves
     if dic1 == dic2:
         # print('\n\n*********Son anagramas***********\n\n')
         return True
@@ -25,23 +25,21 @@ def anagram(frase1, frase2):
 
 
 def create_dic(frase):
-    '''
+    """
     toma la frase y la combierte en un diccionario usando la letra como llave y
      sumando la cantidad de veces que se repite cada letra como valor
     :param frase: trae la frace a convertir a diccionario
     :return: regresa el diccionario
-    '''
-    #esta declaracion es para que el diccionario exista fuera del for
+    """
+    # esta declaracion es para que el diccionario exista fuera del for
     dictionary = {}
     # aqui estoy quitando los espacios vacios y los guiones del string
     frase = frase.replace(' ', '').replace('-', '').lower()
     for i in frase:
-        #reviso si la letra en i ya existe en el diccionario si es asi le agrego uno al valor actual,
+        # reviso si la letra en i ya existe en el diccionario si es asi le agrego uno al valor actual,
         # de lo contrario inicializo la llave en uno
         if i in dictionary.keys():
             dictionary[i] = dictionary[i] + 1
         else:
             dictionary[i] = 1
     return dictionary
-
-
